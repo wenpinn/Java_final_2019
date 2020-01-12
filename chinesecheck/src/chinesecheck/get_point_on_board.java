353a36313584202974f7e64fe05e66863531f396
YuChangHan <0987431104a@gmail.com>
2020-01-12 11:03:47 +0800

    add sth

--- chinesecheck/src/chinesecheck/get_point_on_board.java
+++ chinesecheck/src/chinesecheck/get_point_on_board.java
@@ -3,10 +3,19 @@ package chinesecheck;
 import java.awt.Point;
 
 class get_point_on_board {
-    Point pointa;
-    Point pointb;
+    Point pointa = new Point();
+    Point pointb = new Point();
 
-    public get_point_on_board() {
+    public get_point_on_board(Point a, Point b) {
+        set(a, b);
+    }
 
+    Point getpointa() {
+        return pointa;
+    }
+
+    void set(Point a, Point b) {
+        pointa = a;
+        pointb = b;
     }
 }
